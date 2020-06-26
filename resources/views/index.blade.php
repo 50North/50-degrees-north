2bce65688f6862a547b0e576fa9dd90d873920c6
Chris Beagley <chrisbeagley1@gmail.com>
2020-06-26 21:12:19 +0100

    FIXED REZA STUPID MISTAKE LEFT QUOTE IN

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -72,32 +72,14 @@
 
       <section class="join-main-section"  >
         <div class="call-to-action ">
-          <h1 class="title"  ">START YOUR JOURNEY</h1>
+          <h1 class="title">START YOUR JOURNEY</h1>
          <div>
           
         
       
       </section>
-    <footer class="main-footer">
-      <div class="container">
-        <nav class="footer-nav">
-          <ul>
-            <li><a href="pp.html">Privacy policy</a></li>
-          </ul>
-        </nav>
-
-        <nav class="footer-nav">
-          <ul>
-            <li>
-              <a class="social-link" href="#"><img src="/res/fb.png" /></a>
-            </li>
-            <li>
-              <a class="social-link" href="#"><img src="/res/insta.png" /></a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </footer>
+    <!-- Footer -->
+    @include('includes.footer')
     </main>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
